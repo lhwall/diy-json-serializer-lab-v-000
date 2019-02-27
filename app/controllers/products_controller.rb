f13b507f7ea7dd44ee752b6164c00e9fd02ba5cb
Lucy Wall <lhwall@gmail.com>
2019-02-27 20:38:25 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,10 +3,14 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def show 
+    @product = Product.find(params[:id])
+  end 
+
   def data
     product = Product.find(params[:id])
     render json: ProductSerializer.serialize(product)
-  end
+  end 
 
   def inventory
     product = Product.find(params[:id])
