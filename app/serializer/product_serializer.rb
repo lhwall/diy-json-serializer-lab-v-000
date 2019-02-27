53e0666fed7f5cdad6184a62ca750a58c769100b
Lucy Wall <lhwall@gmail.com>
2019-02-27 20:39:16 +0000

    Automatically backed up by Learn

--- app/serializer/product_serializer.rb
+++ app/serializer/product_serializer.rb
@@ -3,6 +3,7 @@ class ProductSerializer
 
 
   serialized_product = '{'
+  serialized_product += '"id": ""' + product.id.to_s + '"",'
   serialized_product += '"name": ""' + product.name + '"",'
     serialized_product += '"price": ""' + product.price + '"",'
     serialized_product += '"inventory": ""' + product.inventory.to_s + '"",'
